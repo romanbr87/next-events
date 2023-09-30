1e4ba005348cb26c6069fbc596cda9ba68f6780d
Roman Braverman <75772227+romanbr87@users.noreply.github.com>
2023-09-30 19:40:42 +0000

    Update 4 files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import ProductForm from '../components/ProductForm';
+import { Row, Col, Container } from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.css';
 
 const IndexPage: NextPage = () => {
 
   return (
-    <div>
-      <h1>Product Form</h1>
+    <Container>
+    <Row className="mt-2">
+    <Col lg={3} md={3}></Col>
+    <Col lg={6} md={6}>
+    <div className="well">
+      <h2>Product Form</h2>
       <ProductForm />
     </div>
+    </Col>
+    </Row>
+    </Container>
   );
 };
 
--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -6,7 +6,6 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { Form, Button } from "react-bootstrap";
 import { Products } from "../app/schemas/models/clients/products.schema";
 import * as yup from "yup";
-import "./ProductForm.module.css";
 
 const ProductForm: NextPage = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -35,17 +34,17 @@ const ProductForm: NextPage = () => {
     productTree: yup.array(
       yup.object().shape({
         product: yup.string().required(),
-        quantity: yup.number().required(),
+        quantity: yup.number().required()
       })
-    ),
+    )
   });
 
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors }
   } = useForm<Products>({
-    resolver: yupResolver(productsYupSchema),
+    resolver: yupResolver(productsYupSchema)
   });
 
   const onSubmit = async (data: Products) => {
@@ -68,78 +67,79 @@ const ProductForm: NextPage = () => {
   };
 
   return (
-    <div className="well">
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Form.Group>
-          <Form.Label>Product Number</Form.Label>
-          <Form.Control type="number" {...register("productNum")} />
-          {errors.productNum && (
-            <span className="text-danger">{errors.productNum.message}</span>
-          )}
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" {...register("name")} />
-          {errors.name && (
-            <span className="text-danger">{errors.name.message}</span>
-          )}
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Price</Form.Label>
-          <Form.Control type="number" {...register("price")} />
-          {errors.price && (
-            <span className="text-danger">{errors.price.message}</span>
-          )}
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Stock</Form.Label>
-          <Form.Control type="number" {...register("stock")} />
-          {errors.stock && (
-            <span className="text-danger">{errors.stock.message}</span>
-          )}
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Average Cost</Form.Label>
-          <Form.Control type="number" {...register("avgCost")} />
-          {errors.avgCost && (
-            <span className="text-danger">{errors.avgCost.message}</span>
-          )}
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Is Atomic</Form.Label>
-          <Form.Control type="checkbox" {...register("isAtomic")} />
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Categories</Form.Label>
-          <Form.Control type="textarea" {...register("categories")} />
-          {errors.categories && (
-            <span className="text-danger">{errors.categories.message}</span>
-          )}
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Product Tree</Form.Label>
-          <Form.Control type="textarea" {...register("productTree")} />
-          {errors.productTree && (
-            <span className="text-danger">{errors.productTree.message}</span>
-          )}
-        </Form.Group>
-
-        <Button type="submit" disabled={isSubmitting}>
-          Submit
-        </Button>
-
-        {successMessage && (
-          <div className="alert alert-success">{successMessage}</div>
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Form.Group className="mb-2">
+        <Form.Label>Product Number</Form.Label>
+        <Form.Control type="number" min={0} {...register("productNum")} />
+        {errors.productNum && (
+          <span className="text-danger">{errors.productNum.message}</span>
         )}
-      </Form>
-    </div>
+      </Form.Group>
+
+      <Form.Group className="mb-2">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" {...register("name")} />
+        {errors.name && (
+          <span className="text-danger">{errors.name.message}</span>
+        )}
+      </Form.Group>
+
+      <Form.Group className="mb-2">
+        <Form.Label>Price</Form.Label>
+        <Form.Control type="number" min={0} {...register("price")} />
+        {errors.price && (
+          <span className="text-danger">{errors.price.message}</span>
+        )}
+      </Form.Group>
+
+      <Form.Group className="mb-2">
+        <Form.Label>Stock</Form.Label>
+        <Form.Control type="number" min={0} {...register("stock")} />
+        {errors.stock && (
+          <span className="text-danger">{errors.stock.message}</span>
+        )}
+      </Form.Group>
+
+      <Form.Group className="mb-2">
+        <Form.Label>Average Cost</Form.Label>
+        <Form.Control type="number" min={0} {...register("avgCost")} />
+        {errors.avgCost && (
+          <span className="text-danger">{errors.avgCost.message}</span>
+        )}
+      </Form.Group>
+
+      <Form.Group className="mb-2">
+        <Form.Check
+          label="Is Atomic?"
+          type="checkbox"
+          {...register("isAtomic")}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-2">
+        <Form.Label>Categories</Form.Label>
+        <Form.Control type="textarea" {...register("categories")} />
+        {errors.categories && (
+          <span className="text-danger">{errors.categories.message}</span>
+        )}
+      </Form.Group>
+
+      <Form.Group className="mb-2">
+        <Form.Label>Product Tree</Form.Label>
+        <Form.Control type="textarea" {...register("productTree")} />
+        {errors.productTree && (
+          <span className="text-danger">{errors.productTree.message}</span>
+        )}
+      </Form.Group>
+
+      <Button type="submit" className="mt-2" disabled={isSubmitting}>
+        Submit
+      </Button>
+
+      {successMessage && (
+        <div className="alert alert-success">{successMessage}</div>
+      )}
+    </Form>
   );
 };
 
